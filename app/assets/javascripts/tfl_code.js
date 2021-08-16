857e8949feae6c1b758198663a33987116f714d0
Sandy Gudgeon <alexander.gudgeon@justice.gov.uk>
2021-08-16 20:48:59 +0100

    run standard --fix

--- app/assets/javascripts/tfl_code.js
+++ app/assets/javascripts/tfl_code.js
@@ -1,23 +1,25 @@
 // Initialise constant
-const cstartBalance = 30.00;
+ const cstartBalance = 30.00
 // Initialise variables
-let _endBalance = 27.20;
+let _endBalance = 27.20
 // let _traveType = "Bus";
 // let _startStation = '';
 // let _endStation = '';
+let _startBalance = 30.00;
+let _cost = 2.80;
 
-//Show start/end balance & cost
-_startBalance = cstartBalance;
-_endBalance = _startBalance - _cost;
+// Show start/end balance & cost
+_startBalance = cstartBalance
+_endBalance = _startBalance - _cost
 // import currency = require("currency.js");
 // const UKP = value => currency(value, { symbol: '£', decimal: '.', separator: ',' });
 
 //  create number formatter
-let _formatter = new Intl.NumberFormat(undefined, {
+const _formatter = new Intl.NumberFormat(undefined, {
   style: 'currency',
   currency: 'UKP'
-});
+})
 
-let _formatted_startbalance = _formatter.format(_startBalance);
-let _formatted_cost = _formatter.format(_cost);
-let _formatted_endbalance = _formatter.format(_endBalance);
+const _formatted_startbalance = _formatter.format(_startBalance)
+const _formatted_cost = _formatter.format(_cost)
+const _formatted_endbalance = _formatter.format(_endBalance)
