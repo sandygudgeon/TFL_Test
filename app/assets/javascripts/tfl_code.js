15a9d64244118e1703cebc722a288850ea6464fb
Sandy Gudgeon <alexander.gudgeon@justice.gov.uk>
2021-08-16 20:51:33 +0100

    Update tfl_code.js

--- app/assets/javascripts/tfl_code.js
+++ app/assets/javascripts/tfl_code.js
@@ -6,7 +6,9 @@ let _endBalance = 27.20;
 let _traveType = 'Bus';
 let _startStation = 'xxx';
 let _endStation = 'yyy';
-
+let _startBalance = 30.00;
+let _cost = 2.80;
+let _endBalance = 27.20;
 // Show start/end balance & cost
 _startBalance = cstartBalance;
 _endBalance = _startBalance - _cost;
