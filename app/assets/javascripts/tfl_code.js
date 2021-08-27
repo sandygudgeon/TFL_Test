8f0a1263973ebbff1fbebfa666511d1a51f14ed2
Sandy Gudgeon <alexander.gudgeon@justice.gov.uk>
2021-08-27 14:08:27 +0100

    Moved code into sandy.html

--- app/assets/javascripts/tfl_code.js
+++ app/assets/javascripts/tfl_code.js
@@ -1,16 +1,2 @@
-// create number formatter
-let _startBalance = 30.00;
-let _cost = 2.80;
-let _endBalance = 27.20;
-
-let _formatter = new Intl.NumberFormat(undefined, {
-  style: 'currency',
-  currency: 'UKP'
-});
-
-let _formatted_startBalance = _formatter.format(_startBalance);
-let _formatted_cost = _formatter.format(_cost);
-let _formatted_endBalance = _formatter.format(_endBalance);
-
 
 
